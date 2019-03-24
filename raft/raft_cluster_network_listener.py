3aecd1d381105f21585f4119c57c9ead02506284
Syed Abbas <Syed-Abbas@gmail.com>
2019-03-24 17:16:44 -0500

    adds peet network listener

---
+++ raft/raft_cluster_network_listener.py
@@ -0,0 +1,58 @@
+import socket
+import sys
+import select
+import struct
+import json
+
+class RaftClusterNetworkListener:
+    def __init__(self, server_address, node_id, message_queue):
+        self._server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._server.setblocking(0)
+        self._server_address = server_address
+        self._node_id = node_id
+        self._message_queue = message_queue
+
+        #TODO make configurable
+        self._timeout_seconds = 1
+        self._backlog = 5
+        self._message_header_len = 4
+
+    def run(self):
+        print(f'RaftListener for node_id {self._node_id} starting up', file=sys.stdout)
+        self._listen()
+
+    def _listen(self):
+        self._server.bind(self._server_address)
+        self._server.listen(5)
+        inputs = [self._server]
+        self._message_queue.put(b'Starting up')
+        while inputs:
+            readable, writable, exceptional = select.select(inputs, [], inputs, self._timeout_seconds)
+            for s in readable:
+                if s is self._server:
+                    connection, _ = s.accept()
+                    inputs.append(connection)
+                else:
+                    content_len = self._read_content_len(s)
+                    if content_len < 0:
+                        inputs.remove(s)
+                        s.close()
+                    else:
+                        content = self._read_content(s, content_len)
+                        data = self._parse_content(content)
+                        self._message_queue.put(data)
+
+    def _read_content_len(self, s):
+        header =  s.recv(self._message_header_len)
+        if header:
+            return struct.unpack('>I', header)[0]
+        else:
+            return -1
+
+    def _read_content(self, s, content_len):
+        content = s.recv(content_len)
+        return content
+
+    def _parse_content(self, content):
+        return json.loads(content.decode())
+
---
+++ tests/test_raft_cluster_network_listener.py
@@ -0,0 +1,62 @@
+import multiprocessing as mp
+import queue
+import socket
+import struct
+import json
+
+import pytest
+
+from raft.raft_cluster_network_listener import RaftClusterNetworkListener
+
+def test_raft_cluster_network_listener(message_queue):
+    """
+    it reads, parses and enques for processing JSON messages received from peers 
+    """
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect(('localhost', 5000))
+    s2.connect(('localhost', 5000))
+    msg = {
+        'message_type': 'AppendEntries',
+        'args': {
+            'term': 1,
+            'leader_id': 123,
+            'prev_log_index': 23,
+            'prev_log_term': 0,
+            'entries': ['set x 1'],
+            'leader_commit': 24
+        }
+    }
+    msg_bytes = json.dumps(msg).encode()
+    payload = struct.pack('>I', len(msg_bytes)) + msg_bytes
+    s.send(payload)
+    s.send(payload)
+    s2.send(payload)
+    try:
+        item = message_queue.get(timeout=5)
+    except queue.Empty:
+        s.close()
+        pytest.fail('Raftlistener failed to communicate within allotted timeout of 5 seconds')
+    s.close()
+
+    assert item == msg
+
+
+@pytest.fixture(name='message_queue')
+def setup_raft_listener():
+    q = mp.Queue()
+    p = mp.Process(target=start_listener, args=(q,))
+    p.start()
+    try:
+        q.get(timeout=3)
+    except queue.Empty:
+        p.kill()
+        pytest.fail('Raftlistener failed to start within alloted timeout of 3 seconds ')
+
+    yield q
+
+    p.kill()
+
+def start_listener(message_queue):
+    listener = RaftClusterNetworkListener(('localhost', 5000), 1, message_queue)
+    listener.run()
